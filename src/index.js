a7702cb997ab1be40132e1699618f8c4af97ea44
Gabriel Laroche <gabriel.laroche@hey.com>
2020-08-06 15:20:23 -0400

    Update countdown for mainstream support of IE11
    https://www.swyx.io/writing/ie11-eol/

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import countdown from "./js/countdown";
 import populateReasonsAndWebsites from './js/reasonsAndWebsites';
 
-countdown('10/14/2025 05:00:00 PM');
+countdown('10/30/2020 05:00:00 PM');
 populateReasonsAndWebsites();
