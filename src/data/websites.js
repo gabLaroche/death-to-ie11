af3dba78ec96d598d29eecdb1abb13091ce99424
Nazario A. Ayala <nazario.ayala@rallyhealth.com>
2020-08-18 12:15:03 -0700

    added office 365

--- src/data/websites.js
+++ src/data/websites.js
@@ -64,5 +64,9 @@ export default {
             "url": "https://blog.getbootstrap.com/2020/06/16/bootstrap-5-alpha/",
             "label": "Bootstrap 5"
         },
+        {
+            "url": "https://techcommunity.microsoft.com/t5/microsoft-365-blog/microsoft-365-apps-say-farewell-to-internet-explorer-11-and/ba-p/1591666",
+            "label": "Microsoft 365"
+        },
     ]
 }
