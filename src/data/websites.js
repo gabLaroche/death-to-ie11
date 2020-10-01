d5cf1033b77416819c5afe9ab931413cb18554d7
Sahil <sahilkumarpcm@gmail.com>
2020-10-01 16:48:08 +0530

    format-changes

--- src/data/websites.js
+++ src/data/websites.js
@@ -1,108 +1,94 @@
 export default {
-  list: [
-    {
-      url: "https://dev.to/",
-      label: "Dev.to",
-    },
-    {
-      url:
-        "https://help.github.com/en/github/getting-started-with-github/supported-browsers",
-      label: "GitHub",
-    },
-    {
-      url:
-        "https://meta.stackexchange.com/questions/56161/which-browsers-are-officially-supported-and-what-else-do-i-need",
-      label: "Stack Overflow (and other Stack Exchange sites)",
-    },
-    {
-      url:
-        "https://support.zendesk.com/hc/en-us/articles/360002084367-Removal-of-support-for-Internet-Explorer-11",
-      label: "Zendesk",
-    },
-    {
-      url: "https://protonmail.com",
-      label: "ProtonMail",
-    },
-    {
-      url:
-        "https://community.atlassian.com/t5/Feedback-Forum-articles/Retiring-IE11-support-for-Atlassian-cloud-server-and-data-center/ba-p/1185312",
-      label: "Atlassian",
-    },
-    {
-      url: "https://support.google.com/youtube/answer/175292?hl=en",
-      label: "YouTube",
-    },
-    {
-      url:
-        "https://help.meetup.com/hc/en-us/articles/360002965711-What-browsers-does-Meetup-support-",
-      label: "Meetup",
-    },
-    {
-      url: "https://slack.com/help/articles/115002037526",
-      label: "Slack",
-    },
-    {
-      url:
-        "https://help.trello.com/article/735-what-browsers-and-mobile-platforms-does-trello-support",
-      label: "Trello",
-    },
-    {
-      url:
-        "https://support.discordapp.com/hc/en-us/articles/213491697-What-are-the-OS-system-requirements-for-Discord-",
-      label: "Discord",
-    },
-    {
-      url: "https://support.spotify.com/us/using_spotify/the_basics/webplayer/",
-      label: "Spotify",
-    },
-    {
-      url:
-        "https://support.wix.com/en/article/wix-compatibility-with-internet-explorer",
-      label: "Wix Editor and ADI Builder",
-    },
-    {
-      url: "https://help.yahoo.com/kb/SLN4556.html?guccounter=1",
-      label: "Yahoo",
-    },
-    {
-      url:
-        "https://www.linkedin.com/help/linkedin/answer/4135/supported-internet-browsers-for-linkedin",
-      label: "LinkedIn",
-    },
-    {
-      url: "https://blog.getbootstrap.com/2020/06/16/bootstrap-5-alpha/",
-      label: "Bootstrap 5",
-    },
-    {
-      url:
-        "https://techcommunity.microsoft.com/t5/microsoft-365-blog/microsoft-365-apps-say-farewell-to-internet-explorer-11-and/ba-p/1591666",
-      label: "Microsoft 365",
-    },
-    {
-      url:
-        "https://techcommunity.microsoft.com/t5/microsoft-365-blog/microsoft-365-apps-say-farewell-to-internet-explorer-11-and/ba-p/1591666",
-      label: "Microsoft Teams",
-    },
-    {
-      url:
-        "https://help.twitter.com/fr/using-twitter/twitter-supported-browsers",
-      label: "twitter",
-    },
-    {
-      url:
-        "https://support.zoom.us/hc/en-us/articles/214629443-Zoom-web-client",
-      label: "zoom",
-    },
+  "list" : [
+      {
+          "url" : "https://dev.to/",
+          "label": "Dev.to"
+      },
+      {
+          "url" : "https://help.github.com/en/github/getting-started-with-github/supported-browsers",
+          "label": "GitHub"
+      },
+      {
+          "url" : "https://meta.stackexchange.com/questions/56161/which-browsers-are-officially-supported-and-what-else-do-i-need",
+          "label": "Stack Overflow (and other Stack Exchange sites)"
+      },
+      {
+          "url" : "https://support.zendesk.com/hc/en-us/articles/360002084367-Removal-of-support-for-Internet-Explorer-11",
+          "label": "Zendesk"
+      },
+      {
+          "url" : "https://protonmail.com",
+          "label": "ProtonMail"
+      },
+      {
+          "url" : "https://community.atlassian.com/t5/Feedback-Forum-articles/Retiring-IE11-support-for-Atlassian-cloud-server-and-data-center/ba-p/1185312",
+          "label": "Atlassian"
+      },
+      {
+          "url" : "https://support.google.com/youtube/answer/175292?hl=en",
+          "label": "YouTube"
+      },
+      {
+          "url" : "https://help.meetup.com/hc/en-us/articles/360002965711-What-browsers-does-Meetup-support-",
+          "label": "Meetup"
+      },
+      {
+          "url" : "https://slack.com/help/articles/115002037526",
+          "label": "Slack"
+      },
+      {
+          "url" : "https://help.trello.com/article/735-what-browsers-and-mobile-platforms-does-trello-support",
+          "label": "Trello"
+      },
+      {
+          "url" : "https://support.discordapp.com/hc/en-us/articles/213491697-What-are-the-OS-system-requirements-for-Discord-",
+          "label": "Discord"
+      },
+      {
+          "url" : "https://support.spotify.com/us/using_spotify/the_basics/webplayer/",
+          "label": "Spotify"
+      },
+      {
+          "url" : "https://support.wix.com/en/article/wix-compatibility-with-internet-explorer",
+          "label": "Wix Editor and ADI Builder"
+      },
+      {
+          "url" : "https://help.yahoo.com/kb/SLN4556.html?guccounter=1",
+          "label": "Yahoo"
+      },
+      {
+          "url" : "https://www.linkedin.com/help/linkedin/answer/4135/supported-internet-browsers-for-linkedin",
+          "label" : "LinkedIn"
+      },
+      {
+          "url": "https://blog.getbootstrap.com/2020/06/16/bootstrap-5-alpha/",
+          "label": "Bootstrap 5"
+      },
+      {
+          "url": "https://techcommunity.microsoft.com/t5/microsoft-365-blog/microsoft-365-apps-say-farewell-to-internet-explorer-11-and/ba-p/1591666",
+          "label": "Microsoft 365"
+      },
+      {
+          "url": "https://techcommunity.microsoft.com/t5/microsoft-365-blog/microsoft-365-apps-say-farewell-to-internet-explorer-11-and/ba-p/1591666",
+          "label": "Microsoft Teams"
+      },
+      {
+          "url": "https://help.twitter.com/fr/using-twitter/twitter-supported-browsers",
+          "label": "twitter"
+      },
+      {
+          "url": "https://support.zoom.us/hc/en-us/articles/214629443-Zoom-web-client",
+          "label": "zoom"
+      },
+      
+      {
+          "url": "https://answers.microsoft.com/en-us/ie/forum/all/websites-telling-me-they-will-no-longer-support/5caf94d0-facd-43db-93d0-4745feec10b7",
+          "label": "facebook"
+      },
+      {
+          "url": "https://help.salesforce.com/articleView?id=getstart_browsers_sfx.htm&type=5",
+          "label": "salesforce",
+      }
+  ]
+}
 
-    {
-      url:
-        "https://answers.microsoft.com/en-us/ie/forum/all/websites-telling-me-they-will-no-longer-support/5caf94d0-facd-43db-93d0-4745feec10b7",
-      label: "facebook",
-    },
-    {
-      url:
-        "https://help.salesforce.com/articleView?id=getstart_browsers_sfx.htm&type=5",
-      label: "salesforce",
-    },
-  ],
-};
