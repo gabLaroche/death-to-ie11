be61d96408286831d939dbdd2d8edb8cb39d642c
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-10-06 20:26:30 +0530

    style: new UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import countdown from "./js/countdown";
-import populateReasonsAndWebsites from './js/reasonsAndWebsites';
+import populateReasonsAndWebsites from "./js/reasonsAndWebsites";
 
 countdown(
-    '08/17/2021 05:00:00 PM',
-    'mainstreamTimer',
-    'Mainstream support for Internet Explorer 11 will be dropped in :',
-    'Mainstream support for Internet Explorer 11 has ended for :');
+  "08/17/2021 05:00:00 PM",
+  "mainstreamTimer",
+  "Mainstream support ends in",
+  "Mainstream support has ended for :"
+);
 countdown(
-    '10/14/2025 05:00:00 PM',
-    'extendedTimer',
-    'Extended support for (Windows 10) Internet Explorer 11 will be dropped in :',
-    'Internet Explorer is finally dead and has been dead for :');
+  "10/14/2025 05:00:00 PM",
+  "extendedTimer",
+  "Extended support ends in",
+  "Internet Explorer is finally dead and has been dead for"
+);
 populateReasonsAndWebsites();
