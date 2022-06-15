0233a22ba5dd350abacc81746bdb2d5bd1871d94
rebeccahongsf <rebeccahongsf@gmail.com>
2022-06-15 15:30:26 -0700

    rebeccahongsf | update date time to ISO 8601 format to account for different time zones

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import countdown from "./js/countdown";
 import populateReasonsAndWebsites from "./js/reasonsAndWebsites";
 
 countdown(
-  "06/15/2022 12:52:56 PM",
+  "2022-06-15T13:36:00.000Z",
   "extendedTimer",
   "Support ends in",
   "Internet Explorer is finally dead and has been dead for"
